d1a0c7f23a5ed3f84fc4a19dfb535a4ec2d3154c
Christopher <gomeschristopher@hotmail.com>
2025-04-01 14:17:26 -0300

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContainerComponent } from './shared/container/container.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CardComponent } from './shared/card/card.component';
+import { CardSearchComponent } from './shared/card-search/card-search.component';
+import { CardTestimonyComponent } from './shared/card-testimony/card-testimony.component';
+import { FormSearchComponent } from './shared/form-search/form-search.component';
+import { ModalComponent } from './shared/modal/modal.component';
+import { ControlButtonComponent } from './shared/control-button/control-button.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    HomeComponent,
+    ContainerComponent,
+    FooterComponent,
+    CardComponent,
+    CardSearchComponent,
+    CardTestimonyComponent,
+    FormSearchComponent,
+    ModalComponent,
+    ControlButtonComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +49,15 @@ import { HeaderComponent } from './shared/components/header/header.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/control-button/control-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-control-button',
+  templateUrl: './control-button.component.html',
+  styleUrls: ['./control-button.component.scss']
+})
+export class ControlButtonComponent {
+  @Input() operation: 'increment' | 'decrement' = "increment";
+  @Input() src = '';
+  @Input() alt = '';
+}
